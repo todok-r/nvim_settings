a91f62cd4ea359d1d99797ffb1b17a9820c36eed
Your Name <you@example.com>
2024-06-28 17:33:51 +0900

    migrate init.vim to init.lua

--- init.lua
+++ init.lua
@@ -1,1 +1,26 @@
 require("config.lazy")
+
+vim.opt.mouse = ''
+vim.opt.foldmethod = 'marker'
+vim.opt.visualbell = true
+vim.opt.cursorline = true
+vim.opt.backup = false
+vim.opt.swapfile = false
+vim.opt.grepprg = 'egrep'
+vim.opt.equalalways = true
+
+vim.opt.tabstop=4
+vim.opt.softtabstop=4
+vim.opt.shiftwidth=4
+vim.opt.textwidth=0
+
+vim.opt.smartcase=true
+vim.opt.ignorecase=true
+vim.opt.hlsearch=true
+vim.opt.incsearch=true
+
+vim.api.nvim_set_keymap('', '<F1>', '<ESC>', {})
+vim.api.nvim_set_keymap('c', '<C-B>', '<Left>', {})
+vim.api.nvim_set_keymap('c', '<C-A>', '<Home>', {})
+vim.api.nvim_set_keymap('c', '<C-E>', '<End>', {})
+vim.api.nvim_set_keymap('c',  '<C-D>', '<Delete>', {})
