b832622a77083fc68dc65f3d1da58fb44f886417
Your Name <you@example.com>
2025-01-12 20:39:00 +0900

    modify wrong nvim-treesitter setup

--- lua/plugins/syntax.lua
+++ lua/plugins/syntax.lua
@@ -4,48 +4,52 @@ return {
 	{
 		"nvim-treesitter/nvim-treesitter",
 		build = ":TSUpdate",
-		opts = {
-			ensure_installed = {
-				"bash",
-				"css",
-				"diff",
-				"dockerfile",
-				"html",
-				"javascript",
-				"jsdoc",
-				"json",
-				"latex",
-				"lua",
-				"make",
-				"markdown",
-				"python",
-				"query",
-				"scss",
-				"toml",
-				"tsx",
-				"typescript",
-				"vim",
-				"vimdoc",
-				"vim",
-				"vimdoc",
-				"yaml",
-			},
-			-- Install parsers synchronously (only applied to `ensure_installed`)
-			sync_install = false,
+		config = function()
+			local configs = require("nvim-treesitter.configs")
+			configs.setup({
+				ensure_installed = {
+					"bash",
+					"css",
+					"diff",
+					"dockerfile",
+					"html",
+					"javascript",
+					"jsdoc",
+					"json",
+					"latex",
+					"lua",
+					"make",
+					"markdown",
+					"python",
+					"query",
+					"scss",
+					"sql",
+					"toml",
+					"tsx",
+					"typescript",
+					"vim",
+					"vimdoc",
+					"vim",
+					"vimdoc",
+					"yaml",
+				},
+				-- Install parsers synchronously (only applied to `ensure_installed`)
+				sync_install = false,
 
-			-- Automatically install missing parsers when entering buffer
-			-- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
-			auto_install = true,
+				-- Automatically install missing parsers when entering buffer
+				-- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
+				auto_install = true,
 
-			highlight = {
-				enable = true,
-				-- Setting this to true will run `:h syntax` and tree-sitter at the same time.
-				-- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
-				-- Using this option may slow down your editor, and you may see some duplicate highlights.
-				-- Instead of true it can also be a list of languages
-				additional_vim_regex_highlighting = false,
-			},
-		},
+				highlight = {
+					enable = true,
+					-- Setting this to true will run `:h syntax` and tree-sitter at the same time.
+					-- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
+					-- Using this option may slow down your editor, and you may see some duplicate highlights.
+					-- Instead of true it can also be a list of languages
+					additional_vim_regex_highlighting = false,
+				},
+			})
+		end,
 	},
 	{
 		"lukas-reineke/indent-blankline.nvim",
@@ -90,32 +94,6 @@ return {
 					enable_close = true, -- Auto close tags
 					enable_rename = true, -- Auto rename pairs of tags
 					enable_close_on_slash = true, -- Auto close on trailing </
-				},
-			})
-		end,
-	},
-	{
-		"nvim-treesitter/playground",
-
-		config = function()
-			require("nvim-treesitter.configs").setup({
-				playground = {
-					enable = true,
-					disable = {},
-					updatetime = 25, -- Debounced time for highlighting nodes in the playground from source code
-					persist_queries = false, -- Whether the query persists across vim sessions
-					keybindings = {
-						toggle_query_editor = "o",
-						toggle_hl_groups = "i",
-						toggle_injected_languages = "t",
-						toggle_anonymous_nodes = "a",
-						toggle_language_display = "I",
-						focus_language = "f",
-						unfocus_language = "F",
-						update = "R",
-						goto_node = "<cr>",
-						show_help = "?",
-					},
 				},
 			})
 		end,
